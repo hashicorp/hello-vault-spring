647d0f945807dbd506d835e16d68ae980b59e469
VinnyHC <vinny.mannello@hashicorp.com>
2022-05-05 10:31:07 -0700

    bump versions

--- sample-app/src/main/java/com/hashicorp/hellovaultspring/controllers/HealthcheckController.java
+++ sample-app/src/main/java/com/hashicorp/hellovaultspring/controllers/HealthcheckController.java
@@ -1,14 +1,20 @@
 package com.hashicorp.hellovaultspring.controllers;
 
+import org.springframework.boot.system.SystemProperties;
+import org.springframework.core.SpringVersion;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthcheckController {
+    String healthMsg =
+            "        Status: OK\n" +
+            "Spring Version: " + SpringVersion.getVersion() + "\n" +
+            "   JDK Version: " + SystemProperties.get("java.version") + "\n";
 
     @GetMapping("/healthcheck")
     public String healthcheck() {
-        return "ok";
+        return healthMsg;
     }
 
 }

db692e54b832bb23300daaa1fedde5e0602cbf17
Daniel Huckins <dhuckins@users.noreply.github.com>
2023-08-30 11:14:31 -0400

    update to work w/ newer java

--- quick-start/run.sh
+++ quick-start/run.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-
+set -euo pipefail
+IFS=$'\n\t'
 
 echo "Starting Vault dev server.."
-container_id=$(docker run --rm --detach -p 8200:8200 -e 'VAULT_DEV_ROOT_TOKEN_ID=dev-only-token' vault)
+container_id=$(docker run --rm --detach -p 8200:8200 -e 'VAULT_DEV_ROOT_TOKEN_ID=dev-only-token' hashicorp/vault:latest)
 
 echo "Running quickstart example."
 mvn clean package
